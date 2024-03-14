5f76d5c7a06afbb09e744840f745c66bb56c4723
zhy61718 <107041399+zhy168@users.noreply.github.com>
2024-03-14 16:16:48 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace form_test_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.formsPlot2 = new ScottPlot.FormsPlot();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.formsPlot1.Location = new System.Drawing.Point(-6, 14);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(1007, 547);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1830, 684);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1699, 943);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 80);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1699, 801);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 28);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "192.168.1.14";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1699, 863);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 28);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "1311";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(38, 671);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(890, 463);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.formsPlot2.Location = new System.Drawing.Point(1078, 31);
+            this.formsPlot2.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(911, 530);
+            this.formsPlot2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2126, 1240);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot2);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private ScottPlot.FormsPlot formsPlot2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ScottPlot;
+using ScottPlot.Drawing.Colormaps;
+using ScottPlot.Plottable;
+
+namespace form_test_1
+{
+ 
+    
+
+    public partial class Form1 : Form
+    {
+        //参数区域
+        double[] Values = new double[30];    
+        Stopwatch sw = Stopwatch.StartNew();
+        private  SPL<double> spl;
+        public static double dis;
+        public  double[] y_value = new double[] { };
+        public  double[] x_value = new double[] { };
+        private void InitData()
+        {
+            spl =  new SPL<double>()
+            {
+                Color = Color.FromArgb(68, 110, 200),
+                MarkerSize = 3,
+                Smooth = false
+            };
+            if (spl.Count != 0)
+            {
+                x_value = spl.GetXs().ToArray();
+                y_value = spl.GetYs().ToArray();
+                spl.Clear();
+            }
+            else
+            {
+                spl.Add(DateTime.Now.ToOADate(), 0);
+                spl.AddRange(x_value,y_value);
+                formsPlot1.Plot.Add(spl);
+                formsPlot1.Plot.XAxis.DateTimeFormat(true);
+                formsPlot1.Plot.AxisAuto();
+                formsPlot1.Refresh();
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;  
+            InitData();
+            formsPlot2.Plot.XLabel("时间");
+            formsPlot2.Plot.YLabel("mm");
+            formsPlot2.Plot.Title("distance : mm");
+
+        }
+
+        List<double> datetime = new List<double>();
+        List<double> value1 = new List<double>();
+        //Random random = new Random();
+
+        private void useTCP()
+        {
+            string connectIP = textBox1.Text.Trim();
+            int PORT = int.Parse(textBox2.Text.Trim());
+            Socket _socket = null;
+            byte[] buffer = new byte[4];
+
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress address = IPAddress.Parse(connectIP);
+                IPEndPoint endPoint = new IPEndPoint(address, PORT);
+                _socket.Connect(endPoint);
+                while (true) 
+                {
+                    dis = bytesToInt(buffer, 0);
+                    int length = _socket.Receive(buffer);
+                    richTextBox1.AppendText("成功接收 "+ length.ToString()+ "bytes\n");
+                    richTextBox1.AppendText("distance " + dis.ToString() + "\n");
+
+                    
+                    datetime.Add(DateTime.Now.ToOADate());
+                    formsPlot2.Plot.XAxis.DateTimeFormat(true);
+                    formsPlot2.Plot.AxisAuto();
+                    value1.Add(dis);
+
+                    //value1.Add(random.NextDouble() * 10);
+                    if (value1.Count() > 30)
+                    {
+                        value1.RemoveAt(0);
+                        datetime.RemoveAt(0);
+                    }
+                    formsPlot2.Plot.AddSignalXY(datetime.ToArray(), value1.ToArray(), Color.Red);
+                    richTextBox1.AppendText("List count : " + value1.Count.ToString() + "\n");
+
+                    this.Invoke(new Action(() => { formsPlot2.Refresh(); }));
+                    
+                    //Array.Clear(buffer, 0, buffer.Length);
+                    Thread.Sleep(500);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _socket.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+                value1.Clear();
+                richTextBox1.AppendText("use_SOCKET_ERROR : " + ex.Message);
+
+            }
+
+
+        }
+
+        public static int bytesToInt(byte[] src, int offset = 0)
+        {
+            int value;
+            value = (int)((src[offset] & 0xFF)
+                    | ((src[offset + 1] & 0xFF) << 8)
+                    | ((src[offset + 2] & 0xFF) << 16)
+                    | ((src[offset + 3] & 0xFF) << 24));
+            return value;
+        }
+
+        private void btn_click(object sender, EventArgs e)
+        {
+            Thread thread2 = new Thread(new ThreadStart(useSPL));
+            thread2.Name = "formplot1";
+            thread2.IsBackground = true;
+            thread2.Start();
+
+
+        }
+
+      
+        private void useSPL()
+        {
+            while (true)
+            {
+                double x = DateTime.Now.ToOADate();
+                double y = dis;
+                spl.Add(x, y);
+                if (spl.Count > 10)
+                {
+                    spl.GetXs().RemoveAt(0);
+                    spl.GetYs().RemoveAt(0);
+                }
+                formsPlot1.Plot.AxisAuto();
+                this.Invoke(new Action(() => { formsPlot1.Refresh(); }));
+                Thread.Sleep(500);
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread3 = new Thread(new ThreadStart(useTCP));
+            thread3.Name = "TCP线程1，formplot2";
+            thread3.IsBackground = true;
+            thread3.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            datetime.Clear();
+            value1.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+    public class SPL<T> : ScatterPlotList<T>
+    {
+        public List<T> GetXs()
+        {
+            return Xs;
+        }
+        public List<T> GetYs()
+        {
+            return Ys;
+        }
+    }
+}
